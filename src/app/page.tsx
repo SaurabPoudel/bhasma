e1a5184b87c9f6063eff87bc3c591d6fb91e3795
SaurabPoudel <poudelsaurab20@gmail.com>
2024-11-01 22:13:50 +0545

    warning for haters

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import Footer from "@/components/Footer";
+import Demo from "@/components/landing/Demo";
 import { FeaturesSectionDemo } from "@/components/landing/Features";
 import { Hero } from "@/components/landing/Hero";
+import Warning from "@/components/ui/Warning";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <FeaturesSectionDemo />
+      <Demo />
       <Footer />
+      <Warning />
     </>
   );
 }
